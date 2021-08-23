d640d14ccdd04542469df96867bb45fed57f5961
Alex Bush <bushnoh@gmail.com>
2021-08-23 21:13:04 +1200

    Initial commit

---
+++ src/day1/mod.rs
@@ -0,0 +1,69 @@
+use std::iter;
+
+pub fn run() {
+    let input: Vec<u32> = INPUT
+        .split_whitespace()
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect();
+    let res1 = find_first_to_sum(&input, 2020, 2);
+    match res1 {
+        Some(r) => println!("Answer (part 1): {}", r),
+        None => println!("Did not find an answer"),
+    }
+    let res2 = find_first_to_sum(&input, 2020, 3);
+    match res2 {
+        Some(r) => println!("Answer (part 2): {}", r),
+        None => println!("Did not find an answer"),
+    }
+}
+
+fn find_first_to_sum(input: &Vec<u32>, target: u32, num_elems: usize) -> Option<u64> {
+    let super_input: Vec<&Vec<u32>> = iter::repeat(input).take(num_elems).collect();
+    fn inner_loop(
+        mut acc_input: Vec<&Vec<u32>>,
+        sum_acc: u32,
+        prod_acc: u64,
+        target: u32,
+    ) -> Option<u64> {
+        match acc_input.pop() {
+            None => {
+                if sum_acc == target {
+                    Some(prod_acc)
+                } else {
+                    None
+                }
+            }
+            Some(i_input) => {
+                for i in i_input {
+                    if let Some(u) = (inner_loop)(
+                        acc_input.clone(),
+                        sum_acc + i,
+                        prod_acc * u64::from(*i),
+                        target,
+                    ) {
+                        return Some(u);
+                    }
+                }
+                return None;
+            }
+        }
+    }
+
+    return inner_loop(super_input, 0, 1, target);
+}
+
+const INPUT: &str = include_str!("input.txt");
+
+#[cfg(test)]
+mod tests {
+    use crate::day1;
+
+    #[test]
+    fn test_find_first_to_sum() {
+        assert_eq!(day1::find_first_to_sum(&vec![1, 2, 3], 0, 2), None);
+        assert_eq!(
+            day1::find_first_to_sum(&vec![1721, 979, 366, 299, 675, 1456], 2020, 2),
+            Some(514579)
+        );
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use aoc_2020_rust::day1;
+
+fn main() {
+    day1::run();
+}
