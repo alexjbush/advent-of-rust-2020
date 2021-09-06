3e7f0dbd7e85435f28aba23df6b4cf4623d38697
Alex Bush <bushnoh@gmail.com>
2021-09-06 20:54:40 +1200

    Day5

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -3,7 +3,7 @@ pub struct Day5 {}
 
 impl<'a> Day<'a> for Day5 {
     fn get_tasks(&self) -> Vec<(usize, &dyn Fn() -> String)> {
-        vec![(1, &task)]
+        vec![(1, &task1), (2, &task2)]
     }
 
     fn get_day_number(&self) -> usize {
@@ -11,13 +11,26 @@ impl<'a> Day<'a> for Day5 {
     }
 }
 
-fn task() -> String {
-    INPUT
-        .lines()
-        .map(|p| Pass::decode_pass(p).get_id())
-        .max()
-        .unwrap()
-        .to_string()
+fn task1() -> String {
+    get_ticket_ids().max().unwrap().to_string()
+}
+
+fn task2() -> String {
+    let mut ids = get_ticket_ids().collect::<Vec<u32>>();
+    ids.sort();
+    let mut prev: Option<u32> = None;
+    for id in ids {
+        if id > 1 && prev == Some(id - 2) {
+            return (id - 1).to_string();
+        } else {
+            prev = Some(id)
+        }
+    }
+    "ERROR".to_string()
+}
+
+fn get_ticket_ids() -> Box<dyn Iterator<Item = u32>> {
+    Box::new(INPUT.lines().map(|p| Pass::decode_pass(p).get_id()))
 }
 
 const INPUT: &str = include_str!("input.txt");
