50734eba227ab0e3507875778cf6443a0c560129
Alex Bush <bushnoh@gmail.com>
2021-10-17 19:09:38 +1300

    Day 8 part 2

--- src/day8/mod.rs
+++ src/day8/mod.rs
@@ -6,7 +6,7 @@ pub struct Day8 {}
 
 impl<'a> Day<'a> for Day8 {
     fn get_tasks(&self) -> Vec<(usize, &dyn Fn() -> String)> {
-        vec![(1, &task1)]
+        vec![(1, &task1), (2, &task2)]
     }
 
     fn get_day_number(&self) -> usize {
@@ -16,14 +16,20 @@ impl<'a> Day<'a> for Day8 {
 
 fn task1() -> String {
     let mut parsed = Instructions::parse(INPUT.lines());
-    parsed.iterate_until_loop();
+    parsed.iterate_until_terminated();
+    parsed.acc.to_string()
+}
+
+fn task2() -> String {
+    let mut parsed = Instructions::parse(INPUT.lines());
+    parsed.iterate_until_terminated_with_corruption();
     parsed.acc.to_string()
 }
 
 const INPUT: &str = include_str!("input.txt");
 
 enum Instruction {
-    NoOp,
+    NoOp(i32),
     Acc(i32),
     Jump(i32),
 }
@@ -37,7 +43,7 @@ impl Instruction {
             .map(|v| if sign == "+" { v } else { v * -1 })
             .unwrap();
         match op {
-            "nop" => NoOp,
+            "nop" => NoOp(num),
             "acc" => Acc(num),
             "jmp" => Jump(num),
             _ => panic!(""),
@@ -60,15 +66,21 @@ impl Instructions {
         }
     }
 
-    fn next_is_valid(&self) -> bool {
-        !self.instructions.get(self.line).unwrap().1
+    fn get_state(&self) -> NextState {
+        if self.line >= self.instructions.len() {
+            NextState::Terminated
+        } else if self.instructions.get(self.line).unwrap().1 {
+            NextState::Loop
+        } else {
+            NextState::Valid
+        }
     }
 
     fn next(&mut self) -> () {
         let this = self.instructions.get_mut(self.line).unwrap();
         this.1 = true;
         match this.0 {
-            NoOp => self.line += 1,
+            NoOp(_) => self.line += 1,
             Acc(num) => {
                 self.acc += num;
                 self.line += 1;
@@ -80,15 +92,65 @@ impl Instructions {
         }
     }
 
-    fn iterate_until_loop(&mut self) -> () {
+    fn iterate_until_terminated(&mut self) -> bool {
         loop {
-            if self.next_is_valid() {
-                self.next();
-            } else {
-                break;
+            match self.get_state() {
+                NextState::Loop => return false,
+                NextState::Terminated => return true,
+                NextState::Valid => self.next(),
             }
         }
     }
+
+    fn negate_instruction_at_line(&mut self, line: usize) -> bool {
+        let this = self.instructions.get_mut(line).unwrap();
+        match this.0 {
+            NoOp(num) => {
+                this.0 = Jump(num);
+                return true;
+            }
+            Acc(_) => {
+                return false;
+            }
+            Jump(num) => {
+                this.0 = NoOp(num);
+                return true;
+            }
+        }
+    }
+
+    fn reset_state(&mut self) -> () {
+        self.instructions.iter_mut().for_each(|e| e.1 = false);
+        self.acc = 0;
+        self.line = 0;
+    }
+
+    fn iterate_until_terminated_with_corruption(&mut self) -> () {
+        let mut mutate_line: usize = 0;
+        let mut clean_run: bool = false;
+        loop {
+            let changed = self.negate_instruction_at_line(mutate_line);
+            if !changed && !clean_run {
+                clean_run = true;
+            } else if !changed {
+                mutate_line += 1;
+                continue;
+            }
+            let res = self.iterate_until_terminated();
+            self.negate_instruction_at_line(mutate_line);
+            mutate_line += 1;
+            if res {
+                break;
+            }
+            self.reset_state();
+        }
+    }
+}
+
+pub enum NextState {
+    Valid,
+    Loop,
+    Terminated,
 }
 
 #[cfg(test)]
@@ -105,6 +167,16 @@ acc +1
 jmp -4
 acc +6";
 
+    const TEST2_INPUT: &str = "nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+nop -4
+acc +6";
+
     #[test]
     fn test_parse() {
         let parsed = Instructions::parse(TEST1_INPUT.lines());
@@ -114,7 +186,23 @@ acc +6";
     #[test]
     fn test_task_1() {
         let mut parsed = Instructions::parse(TEST1_INPUT.lines());
-        parsed.iterate_until_loop();
+        parsed.iterate_until_terminated();
         assert_eq!(parsed.acc, 5);
+    }
+
+    #[test]
+    fn test_task_2_loop() {
+        let mut parsed = Instructions::parse(TEST1_INPUT.lines());
+        let res = parsed.iterate_until_terminated();
+        assert_eq!(parsed.acc, 5);
+        assert!(!res)
+    }
+
+    #[test]
+    fn test_task_2_terminate() {
+        let mut parsed = Instructions::parse(TEST2_INPUT.lines());
+        let res = parsed.iterate_until_terminated();
+        assert_eq!(parsed.acc, 8);
+        assert!(res)
     }
 }
